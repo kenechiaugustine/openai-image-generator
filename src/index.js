7ab32fd883fb55e583e395cbc0bcf4efedc13f14
Kene Arionye <kenechiaugustine@gmail.com>
2022-12-21 04:46:11 +0100

    sys info

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const os = require('os');
 const env = require('dotenv').config()
 const express = require('express')
 const OpenAiRouter = require('./routes/openai.routes')
@@ -13,6 +14,28 @@ app.use(express.urlencoded({
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/openai', OpenAiRouter)
+
+app.get('/getSysInfo', (req, res) => {
+    
+    const interfaces = os.networkInterfaces();
+    const addresses = [];
+    for (let k in interfaces) {
+        for (let k2 in interfaces[k]) {
+            let address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+                addresses.push(address.address);
+            }
+        }
+    }
+
+    const userOS = os.platform();
+
+    res.json({
+        Os: userOS,
+        IpAddress: addresses
+    })
+    
+})
 
 
 const PORT = process.env.PORT || 5000
