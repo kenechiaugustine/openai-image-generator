32e841f890f7d6a4d5c80d676b0cca543bbc67f6
kenechiaugustine <kenechiaugustine@gmail.com>
2023-01-12 15:09:52 +0100

    code format

--- src/index.js
+++ src/index.js
@@ -1,51 +1,54 @@
-require('dotenv').config()
+/** @format */
+
+require('dotenv').config();
 const os = require('os');
-const express = require('express')
-const OpenAiRouter = require('./routes/openai.routes')
-const path = require('path')
+const express = require('express');
+const OpenAiRouter = require('./routes/openai.routes');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: false
-}))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
-app.use(express.static(path.join(__dirname, 'public')))
+// 31332
 
-app.use('/openai', OpenAiRouter)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/openai', OpenAiRouter);
 
 app.get('/sysInfo', (req, res) => {
-    
-    const interfaces = os.networkInterfaces();
-    const addresses = [];
-    for (let k in interfaces) {
-        for (let k2 in interfaces[k]) {
-            let address = interfaces[k][k2];
-            if (address.family === 'IPv4' && !address.internal) {
-                addresses.push(address.address);
-            }
-        }
+  const interfaces = os.networkInterfaces();
+  const addresses = [];
+  for (let k in interfaces) {
+    for (let k2 in interfaces[k]) {
+      let address = interfaces[k][k2];
+      if (address.family === 'IPv4' && !address.internal) {
+        addresses.push(address.address);
+      }
     }
+  }
 
-    const userOS = os.platform();
+  const userOS = os.platform();
 
-    res.json({
-        Os: userOS,
-        IpAddress: addresses
-    })
-    
-})
+  res.json({
+    Os: userOS,
+    IpAddress: addresses,
+  });
+});
 
 app.all('*', (req, res) => {
-    res.json({
-        errorCode: "404",
-        message: "Page not found"
-    })
-})
+  res.json({
+    errorCode: '404',
+    message: 'Page not found',
+  });
+});
 
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on PORT: ${PORT}`)
-})
+  console.log(`Server running on PORT: ${PORT}`);
+});
